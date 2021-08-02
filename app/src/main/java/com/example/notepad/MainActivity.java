6b9ff22ef495e267fa78125f9bab8c63f6cd2d10
minju0213 <mjni1214@naver.com>
2021-08-02 19:42:18 +0900

    '기능 설정 텍스트'추가함 8/1 '멤모 로고 사진'추가함 , 'EditText','EditText 테두리'추가함 8/2 'font_functions' java,xml 파일 추가함

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.notepad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    
-    EditText Textbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        Textbox = findViewById(R.id.Textbox);
+
+        Button Text = (Button)
+                findViewById(R.id.Text);
+        Text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,font_functions.class);
+                startActivity(intent);
+            }
+        });
+
+        Button name_Text = (Button) findViewById(R.id.Text);
+        name_Text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, font_functions.class);
+                startActivity(intent);
+            }
+        });
     }
 }
