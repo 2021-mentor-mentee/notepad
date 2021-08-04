311b2318780afc749e01bcf52656f62a883aecff
roncan1 <carugame12_@naver.com>
2021-08-04 20:55:06 +0900

    병합

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -14,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         Textbox = findViewById(R.id.Textbox);
+
+        Button Text = (Button)
+                findViewById(R.id.Text);
+        Text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,font_functions.class);
+                startActivity(intent);
+            }
+        });
+
+        Button name_Text = (Button) findViewById(R.id.Text);
+        name_Text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, font_functions.class);
+                startActivity(intent);
+            }
+        });
     }
 }
