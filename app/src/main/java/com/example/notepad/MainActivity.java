5ccb3554053d81985cbeb71c950d8f2902b8e103
Chanmi0205 <chanmi1155@naver.com>
2021-08-07 18:37:23 +0900

    글씨 기능 (미완성)

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -3,23 +3,57 @@ package com.example.notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
-    EditText Textbox;
+
+    private Spinner spinner;
+    private Spinner spinner2;
+    private TextView textbox2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        Textbox = findViewById(R.id.Textbox);
 
-        Button Text = (Button)
+        spinner = (Spinner)findViewById(R.id.spinner);
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+                Button Text = (Button)
                 findViewById(R.id.Text);
         Text.setOnClickListener(new View.OnClickListener() {
             @Override
