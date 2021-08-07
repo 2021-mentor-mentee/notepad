014b6c01de0e2eee453a4ce032dc8b87df318879
Chanmi0205 <chanmi1155@naver.com>
2021-08-07 19:08:07 +0900

    글씨 기능 (미완성)

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -13,44 +13,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner spinner;
-    private Spinner spinner2;
-    private TextView textbox2;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = (Spinner)findViewById(R.id.spinner);
-        spinner2 = (Spinner)findViewById(R.id.spinner2);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-
-        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
 
                 Button Text = (Button)
--- app/src/main/java/com/example/notepad/font_functions.java
+++ app/src/main/java/com/example/notepad/font_functions.java
@@ -3,12 +3,21 @@ package com.example.notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class font_functions extends AppCompatActivity{
+
+public class font_functions<Textview> extends AppCompatActivity{
+
+    private Spinner spinner;
+    private Spinner spiiner2;
+    private Textview textbox2;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.font_functions);
-    }
+
+        }
 }
