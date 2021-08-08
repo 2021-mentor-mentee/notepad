1fd0b0caae1c9a20693f5f05a14c84b4eba32dcf
Chanmi0205 <chanmi1155@naver.com>
2021-08-08 20:30:08 +0900

    글씨 기능 (미완성)

--- app/src/main/java/com/example/notepad/font_functions.java
+++ app/src/main/java/com/example/notepad/font_functions.java
@@ -3,6 +3,9 @@ package com.example.notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
@@ -10,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class font_functions<Textview> extends AppCompatActivity{
+    
 
     private Spinner spinner;
     private Spinner spiiner2;
@@ -18,6 +22,9 @@ public class font_functions<Textview> extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.font_functions);
+
+
+
 
         }
 }
