be2e1135705974d89b15fa242062f075b496ef0c
Chanmi0205 <chanmi1155@naver.com>
2021-08-14 20:36:45 +0900

    글씨 수정 (미완성)

--- app/src/main/java/com/example/notepad/font_functions.java
+++ app/src/main/java/com/example/notepad/font_functions.java
@@ -1,30 +1,61 @@
 package com.example.notepad;
 
+import static android.R.attr.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.R.attr;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+
 public class font_functions<Textview> extends AppCompatActivity{
-    
+
 
     private Spinner spinner;
-    private Spinner spiiner2;
-    private Textview textbox2;
+    private TextView TV_test;
+
+
+
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.font_functions);
 
 
+        spinner = (Spinner)findViewById(R.id.spinner);
+        TV_test = (TextView)findViewById(R.id.TV_test);
 
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TV_test = (TextView) textColor(TV_test);
+
+            }
+
+            private Object textColor(TextView tv_test) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         }
+
+
+
+
 }
