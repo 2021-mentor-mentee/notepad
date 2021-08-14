ea69d2110859971fc654d601d35820ab9068fa25
roncan <65841016+roncan1@users.noreply.github.com>
2021-08-14 20:45:42 +0900

    글씨색 변경

--- app/src/main/java/com/example/notepad/Text.java
+++ app/src/main/java/com/example/notepad/Text.java
@@ -2,23 +2,23 @@ package com.example.notepad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Text extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
-    }
 
-    public void setColorNum(){
-//        색깔버튼 색깔 값 가져와서 텍스트뷰에 값 넣기
-//        setColor(colorNum);
-    }
 
-    public void setColor(String colorNum){
-//        텍스트뷰 값을 가져와서 글씨 색깔 바꾸는 역할
 
     }
+
+
 }
